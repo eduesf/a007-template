fcdce0c28318416a70b94a3de7212e281f0ffb32
Eduardo Fernandes <eduardo.eduesf@gmail.com>
2022-07-18 21:53:27 -0300

    aula 007, mais exercicios de fixação 1 e 2

---
+++ script.js
@@ -0,0 +1,48 @@
+let nome = prompt ("Qual o seu nome?");
+console.log (nome);
+let idade = Number (prompt ("Qual a sua idade?"))
+console.log (idade)
+
+nome = "Ana P"
+idade = 25
+let redeSocial = ("Tinder")
+
+console.log (nome)
+console.log (idade)
+console.log ("O nome dela é", nome, "eu conheci ela",redeSocial, "ela tem", idade, "anos")
+
+let tenhoPet = (true)
+const nomeDopet = "Pingo"
+console.log (tenhoPet)
+console.log (nomeDopet)
+
+console.log (typeof nome)
+console.log (typeof idade)
+console.log (typeof tenhoPet)
+console.log (typeof nomeDopet)
+
+//Exercicio de Fixação 1
+
+const nomeFilme = prompt ("Qual o nome do Filme")
+let duracaoFilme = Number (prompt("Qual a duração desse Filme?"))
+let eTerror = (true)
+console.log (nomeFilme)
+console.log (duracaoFilme)
+console.log (eTerror)
+
+console.log (typeof nomeFilme)
+console.log (typeof duracaoFilme)
+console.log (typeof eTerror)
+
+// Exercicio de Fixação 2
+
+let A = 12;
+let B = 5;
+let C;
+
+C = B 
+B = A
+A = C
+
+console.log ("A",A)
+console.log ("B",B)
